cb5579f0c9e889655f614a18d8372d464bc9db98
Ryan N. Coffee <coffee@slac.stanford.edu>
2025-05-27 22:59:31 -0700

    using cv2

---
+++ README.md
@@ -0,0 +1,7 @@
+For LCLS-1 data processing.
+```bash
+cd CookieBox_pkg
+cat ${HOME}/conda_environments.bash
+source ${HOME}/conda_environments.bash
+anaenv
+```
--- python/checkXtcav.py
+++ python/checkXtcav.py
@@ -1,9 +1,10 @@
-#!/sdf/data/lcls/ds/amo/amox28216/scratch/all_output/
+#!/sdf/group/lcls/ds/ana/sw/conda1/inst/envs/ana-4.0.66-py3/bin/python
 
 import psana
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
 
 def getshape(raw):
@@ -13,16 +14,21 @@ def getshape(raw):
     return nx,ny
 
 def main(expstr,runs):
-    for r in (runstrs):
+    for r in (runs):
         dsourcestr = 'exp=' + expstr + ':run=' + r
         print("running:\t",dsourcestr)
+        print('Need to subtract a running estimate of dark imates')
         ds = psana.DataSource(dsourcestr)
         print(psana.DetNames('detectors'))
         xt = psana.Detector('xtcav')
         images = []
+        residuals = []
         #for nevent,evt in enumerate(ds.events()):
         nevent = 0
-        while nevent < 10:
+        nlimit = 10
+        nskip = 100
+        while nevent < nlimit:
+            #_ = [next(ds.events) for i in range(nskip)]
             evt = next(ds.events())
             raw = xt.raw(evt)
             if raw is not None:
@@ -32,10 +38,14 @@ def main(expstr,runs):
                     row = raw[i]
                     for j in range(ny):
                         im[i,j] = row[j]
-                images += [im]
+                img = np.array(im,dtype=np.uint16)
+                img = cv2.erode(img,kernel=(3,3))
+                img = cv2.blur(img,ksize=(3,3))
+                images += [img]
+                residuals += [np.array(im,dtype=np.uint16) - img]
             nevent += 1
                     
-            plt.imshow(images[0],origin='lower')
+            plt.imshow(images[-1],origin='lower')
             plt.show()
 
 
