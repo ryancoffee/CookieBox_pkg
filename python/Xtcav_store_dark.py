1ac7fd246d56e2a3039a6176abb760db716e6844
Ryan N. Coffee <coffee@slac.stanford.edu>
2025-06-18 12:45:38 -0700

    working dark

--- python/Xtcav_store_dark.py
+++ python/Xtcav_store_dark.py
@@ -25,52 +25,53 @@ def main(expstr,runs):
             ds = psana.DataSource(dsourcestr)
             print(psana.DetNames('detectors'))
             xt = psana.Detector('xtcav')
-            avgimg = np.zeros((1,1),dtype=np.uint16)
+            avgimg = None #np.zeros((1,1),dtype=np.uint16)
             nevent = 0
+            stepevent = 0
+
             # seems to be the limit is 1<<19 shots in dark runs
             nlimit = 1<<12
             navglim_shift = 7
             nskip = 0
             avgidx = 0
             while nevent < nlimit:
-                evt = next(ds.events())
-                _ = [next(ds.events()) for i in range(nskip)]
-                raw = xt.raw(evt)
-                if raw is not None:
-                    nx,ny = utils.getshape(raw)
-                    if nx==1 or ny==1:
-                        next
-                    im = np.zeros((nx,ny),dtype=np.uint16)
-                    if nevent == 0:
-                        avgimg = np.zeros((nx,ny),dtype=np.uint16)
-                    for i in range(nx):
-                        row = raw[i]
-                        for j in range(ny):
-                            im[i,j] = row[j]
-                    im = utils.massage(im)
-                    if nevent%(1<<navglim_shift) == 0:
-                        avgimg = np.copy(im)
-                    else:
-                        avgimg += im
+                try:
+                    evt = next(ds.events())
+                    _ = [next(ds.events()) for i in range(nskip)]
+                    raw = xt.raw(evt)
+                    if raw is not None:
+                        nx,ny = utils.getshape(raw)
+                        im = np.zeros((nx,ny),dtype=np.uint16)
+                        if (avgimg is None) or (stepevent == 0):
+                            avgimg = np.zeros((nx,ny),dtype=np.uint16)
+                        for i in range(nx):
+                            row = raw[i]
+                            for j in range(ny):
+                                im[i,j] = row[j]
+                        im = utils.massage(im)
+                        if nevent%(1<<navglim_shift) == 0:
+                            avgimg = np.copy(im)
+                        else:
+                            avgimg += im
 
-                nevent += 1
-                if nevent%(1<<navglim_shift) == 0:
-                    avgimg >>= navglim_shift 
-                    akey = 'avg_%03i'%avgidx
-                    if akey in list(rungrp.keys()):
-                        del rungrp[akey]
-                    print('storing %s'%akey)
-                    avimds = rungrp.create_dataset(akey,data=avgimg)
-                    avimds.attrs['avgindex'] = avgidx
-                    avimds.attrs['navg'] = 1<<navglim_shift
-                    avgidx += 1
+                    nevent += 1
+                    stepevent += 1
+    
+                    if nevent%(1<<navglim_shift) == 0:
+                        avgimg >>= navglim_shift 
+                        akey = 'avg_%03i'%avgidx
+                        if akey in list(rungrp.keys()):
+                            del rungrp[akey]
+                        print('storing %s'%akey)
+                        avimds = rungrp.create_dataset(akey,data=avgimg)
+                        avimds.attrs['avgindex'] = avgidx
+                        avimds.attrs['navg'] = 1<<navglim_shift
+                        avgidx += 1
+                        stepevent = 0
 
                 
-                    '''
-                    plt.imshow(avgimg,origin='lower')
-                    plt.title('avgindex = %03i'%avimds.attrs['avgindex'])
-                    plt.show()
-                    '''
+                except:
+                    next
     return
 
 if __name__ == '__main__':
