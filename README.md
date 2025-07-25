a6d12f37646a84ea54bba396262c48d24d8fc96a
Ryan Coffee <coffeer76@gmail.com>
2025-07-24 20:21:03 -0700

    updated leveing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 For LCLS-1 data processing, from the root directory (`CookieBox_pkg`)  
 
+
 # Bright Images
 The runscript is in `./runscript.bash` and it sources the `init.bash` and launches the `./python/Xtcav_preproc.py` with darkpath(input), brightpath(output), and expname and bright run numbers.  
 The slurmscript allows batch submitting a list of run numbers as follows...   
@@ -19,6 +20,15 @@ This is now used only if running directly from an interactive node.
 source init.bash
 python3 python/Xtcav_store_dark.py amo86815 58 62 68
 ```
+# Training set composition  
+Now, for choosing wether a new image is added to the training set or discarded, I'm using ```./python/randomSliceWasserstein.py``` like this...   
+```bash
+python3 ./python/randomSliceWasserstien.py /media/coffee/9C33-6BBD/temp_xtcav/amo86815/xtcav_bright_images_69.h5 /media/coffee/9C33-6BBD/temp_xtcav/amo86815/xtcav_bright_images_69.h5 
+```
+and this measures random selection from one file with a rendom selection for either the same or a different file.  
+The so called "leveling" that I'm doing is to choose whether to keep the sample based on its mean random slice rmse with the comparator.  
+In the end, this should be a search through the existing training set and for the minimum rmse and then if that is appropriate, then replace the most well represented sample.
+
 
 ![Compare Plot](./figures/compare58_58.png)  
 Comparing an individual block average image to the run average (mean of block averages) in fun 58.   
--- python/randomSliceWasserstien.py
+++ python/randomSliceWasserstien.py
@@ -8,6 +8,10 @@ import sys
 import math
 
 def main(aname:str,bname:str):
+    samplerows:int = 64
+    maxhist = 1<<11
+    rmse_hist = [0]*((maxhist>>2)+1)
+    rmse_leveled_hist = [0]*((maxhist>>2)+1)
     with h5py.File(aname,'r') as a:
         with h5py.File(bname,'r') as b:
             akeys = [k for k in a['xtcav'].keys()]
@@ -16,13 +20,13 @@ def main(aname:str,bname:str):
             imb = b['xtcav'][bkeys[0]][()]
             random.shuffle(akeys)
             random.shuffle(bkeys)
-            for indx in range(10):
+            for indx in range(min(len(akeys),len(bkeys))):
                 ima = a['xtcav'][akeys[indx]][()]
                 imb = b['xtcav'][bkeys[indx]][()]
                 rows = [i for i in range(ima.shape[0])]
                 random.shuffle(rows)
                 mse:float = float(0)
-                for r in rows[:ima.shape[1]>>4]:
+                for r in rows[:samplerows]:
                     acumsum = np.cumsum(ima[r,:])
                     bcumsum = np.cumsum(imb[r,:])
                     mse += np.mean(np.power(acumsum-bcumsum,int(2)))
@@ -32,9 +36,22 @@ def main(aname:str,bname:str):
                     plt.plot(bcumsum,label = 'b')
                     plt.show()
                     '''
-                print(math.sqrt(mse)/(ima.shape[1]>>4))
+                updateind = min(np.uint16(math.sqrt(mse)/samplerows)>>2,maxhist>>2)
+                rmse_hist[updateind] += 1
+                if rmse_leveled_hist[updateind]==0:
+                    rmse_leveled_hist[updateind] += 1
+                else:
+                    rmse_leveled_hist[updateind] += int(random.uniform(0,1)<1.0/rmse_leveled_hist[updateind])
+
 
         
+        histbins = [(1<<2)*i for i in range((maxhist>>2)+2)]
+        plt.stairs(rmse_hist,histbins,label='histogram')
+        plt.stairs(rmse_leveled_hist,histbins,label='leveled histogram')
+        plt.xlabel('rmse value')
+        plt.ylabel('occurence')
+        plt.legend()
+        plt.show()
         #grab two images
         
         #select random rows
